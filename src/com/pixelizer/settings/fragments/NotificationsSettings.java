d0ee67127b7c79590380f14a02687a221b62ad81
Master <mhmdeveloperofficial@gmail.com>
2022-04-14 11:56:44 +0000

    Revert "Pixelizer: Alert Slider: Add toggle to disable notifications [2/2]"
    This reverts commit 9ed13c9274c920194dce1b22b29c7fa4374a11b5.

--- src/com/pixelizer/settings/fragments/NotificationsSettings.java
+++ src/com/pixelizer/settings/fragments/NotificationsSettings.java
@@ -23,38 +23,25 @@ import com.android.settings.R;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import android.content.ContentResolver;
-import android.content.Context;
-import androidx.preference.Preference;
-import android.content.res.Resources;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.pixelizer.settings.Utils;
 public class NotificationsSettings extends SettingsPreferenceFragment {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
-    private Preference mAlertSlider;
-
-    private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.pixelizer_settings_notifications);
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-        final Resources res = getResources();
+        PreferenceScreen prefScreen = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
-
-        mAlertSlider = (Preference) prefScreen.findPreference(ALERT_SLIDER_PREF);
-        boolean mAlertSliderAvailable = res.getBoolean(
-                com.android.internal.R.bool.config_hasAlertSlider);
-        if (!mAlertSliderAvailable)
-            prefScreen.removePreference(mAlertSlider);
     }
 
     @Override
